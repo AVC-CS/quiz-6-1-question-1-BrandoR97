44a0ac08ad52856460ee9d47396881264bc3a5aa
BrandoR97 <brandorod55@gmail.com>
2024-10-27 21:39:31 -0700

    Q6.1 1st try

--- main.hpp
+++ main.hpp
@@ -1,8 +1,37 @@
+#include <iostream>
+using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
 
-// ******************************
-// Implement all your functions here
-// ******************************
+void getTwoValues(int &begin, int &end) {
+    do {
+        cout << "Enter two integers (begin < end): ";
+        cin >> begin >> end;
+        if (begin >= end) {
+            cout << "Invalid input. Please ensure the first number is less than the second." << endl;
+        }
+    } while (begin >= end);
+}
+
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+int getNextPrime(int begin) {
+    int next = begin + 1;
+    while (!isPrime(next)) {
+        ++next;
+    }
+    return next;
+}
+
+int getPrevPrime(int end) {
+    int prev = end - 1; 
+    while (!isPrime(prev) && prev > 1) {
+        --prev;
+    }
+    return prev;
+}
